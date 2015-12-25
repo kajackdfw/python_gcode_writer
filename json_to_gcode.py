d042702571adc6c985bca784a7f60f9035e1dfa3
Kelly Taylor <kajackdfw@gmail.com>
2015-12-25 00:00:17 -0600

    circle funk

--- json_to_gcode.py
+++ json_to_gcode.py
@@ -6,6 +6,7 @@ import json
 # G19 YZ Plane
 # G20 Inch mode
 # G21 mm mode
+# G54 circle mode
 # G90 absolute coordinates
 # G91 relative mode
 # G94 feed units per minute
@@ -21,8 +22,14 @@ import json
 
 # define the function blocks
 def circle( params, feedRate ):
-    ncLines = "G0 X" + str(params['x']) + " Y" + str(params['y']) + " F40\n"
+    radius = float(params['radius'])
+    ncLines = "G0 X" + str(params['x']) + " Y" + str( float(params['y']) + radius ) + " F40\n"
     ncLines = ncLines + "M3\n"
+    # G02 X0. Y0.5 I0.5 J0. F2.5
+    # X0.5 Y0. I0. J-0.5
+    # X0. Y-0.5 I-0.5 J0.
+    # X-0.5 Y0. I0. J0.5
+    ncLines = ncLines + "G02 X" + str(float(params['x']) + radius ) + " Y" + str( params['y'] ) + " I" + str(radius) + " J0. F2" + str(feedRate)
     ncLines = ncLines + "circle"
     ncLines = ncLines + "M5\n"
     return ncLines
@@ -56,7 +63,7 @@ jstr = str(patternFile.read())
 cuttingOps = json.loads(jstr)
 ncFileComment = str(cuttingOps)
 
-ncFirstLine = "G17 [unit] [mode] G94"
+ncFirstLine = "G17 [unit] [mode] G94 G54"
 
 if cuttingOps['config']['unit'] == 'mm' :
     ncFirstLine = str.replace(ncFirstLine, '[unit]', 'G21')
@@ -87,8 +94,9 @@ for oppNum, opp in cuttingOps['cuts'].iteritems():
     adjusted = []
     opp['tempX'] = float( opp['x'] ) * scale
     opp['tempY'] = float( opp['y'] ) * scale
-    opp['wide'] = float( opp['wide'] ) * scale
-    opp['tall'] = float( opp['tall'] ) * scale
+    if ( opp['shape'] == 'rectangle' ) : opp['wide'] = float( opp['wide'] ) * scale
+    if ( opp['shape'] == 'rectangle' ) : opp['tall'] = float( opp['tall'] ) * scale
+    if ( opp['shape'] == 'circle' ) : opp['radius'] = float( opp['diameter']) * 0.5 * scale
     try:
         cutArray = opp['array'] # is there an array of this shape to process ? If not do once in exception
         opp['column_spacing'] = float(opp['array']['x_spacing']) * scale
@@ -108,9 +116,7 @@ for oppNum, opp in cuttingOps['cuts'].iteritems():
     except KeyError:
         # Array key is not present
         print "exception: missing array element for opp # = " + str(oppNum)
-        ncFile.write( cutShape[opp['shape']]( opp, cuttingOps['config']['cut_speed'] ))
-
-# http://stackoverflow.com/questions/11479816/what-is-the-python-equivalent-for-a-case-switch-statement
+        ncFile.write( cutShape[ opp['shape'] ]( opp, cuttingOps['config']['cut_speed'] ) )
 
 # cut the border
 ncFile.write("M3\n")
