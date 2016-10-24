5585ad02bcefbfc3fac85c1a9991e705e541e680
kajackdfw <kajackdfw@yahoo.com>
2016-10-24 09:41:08 -0500

    command line arguements

--- nc.py
+++ nc.py
@@ -1,10 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json
-from sys import argv
+import sys, getopt
 from pattern import NCPattern
 
+def main(argv):
+    nc_file = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -m <cnc_machine> -p <pattern_file> -n <nc_file>')
+            sys.exit()
+        elif opt in ("-m", "--machine"):
+            machine = arg
+        elif opt in ("-n", "--ncfile"):
+            nc_file = arg
+        elif opt in ("-p", "--pattern"):
+            pattern_file = arg
 
+# Parse the command line arguements
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
+# Process file
 my_pattern = NCPattern('shapeoko_3_xl')
 my_pattern.load('patterns/router/vacuum_table_holes.json')
 my_pattern.generate_nc_code()
