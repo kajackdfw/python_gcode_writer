b276b5be3a6da232fc16ab8e9f408f8783494124
kajackdfw <kajackdfw@yahoo.com>
2016-10-25 16:18:00 -0500

    command line arguements

--- nc.py
+++ nc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import sys
 from pattern import NCPattern
+my_pattern = NCPattern(sys.argv[1:])
 
 # Process file
-my_pattern = NCPattern(sys.argv[1:])
 my_pattern.load()
 my_pattern.generate_nc_code()
 my_pattern.save_nc_data()
