e6cfd6af4b3e28f5c5cd966cdfc8a878aebe6552
kajackdfw <kajackdfw@yahoo.com>
2016-10-26 10:27:00 -0500

    load pattern and machine config

--- pattern.py
+++ pattern.py
@@ -1,4 +1,4 @@
-import sys, getopt
+import sys, getopt, json
 
 
 class NCPattern:
@@ -7,7 +7,10 @@ class NCPattern:
 
         self.nc_file = 'nc/test.nc'
         self.pattern_file = ''
-        self.machine = ''
+        self.machine_file = ''
+
+        self.pattern = {}
+
         try:
             opts, args = getopt.getopt(argv, "", ["machine=", "pattern_file=", "nc_file="])
         except getopt.GetoptError:
@@ -18,25 +21,45 @@ class NCPattern:
                 print('test.py --machine=wazer.json --pattern_file=bird_house.json --nc_file=bird_house.nc')
                 sys.exit()
             elif opt in ("-m", "--machine"):
-                self.machine = arg
+                self.machine_file = arg
             elif opt in ("-n", "--nc_file"):
                 self.nc_file = arg
             elif opt in ("-p", "--pattern_file"):
                 self.pattern_file = arg
 
-        if self.machine == '':
+        if self.machine_file == '':
             print('Error: You must specify a cnc machine!')
             sys.exit(2)
 
-        self.summary = '  NCPattern Class initialized for ' + self.machine + '\n'
+        # Load the machine file if present
+        try:
+            machine_fh = open("machines/" + self.machine_file + ".json", "r")
+            json_array_string = str(machine_fh.read())
+            self.machine = json.loads(json_array_string)
+            self.summary = '  Machine config for ' + self.machine['description'] + ' loaded\n'
+        except IOError:
+            print("  Error : No machine file found for " + self.machine_file + '\n')
+            sys.exit()
+
+        self.summary += '  NCPattern Class initialized \n'
         self.status = 'initialized'
 
     def load(self):
         if self.pattern_file == '':
             print('Error: You must specify a json pattern file!')
             sys.exit(2)
-        self.summary += '  Load ' + self.pattern_file + ' \n'
-        return False
+
+        # Load the pattern file if present
+        try:
+            pattern_fh = open(self.pattern_file, "r")
+            json_array_string = str(pattern_fh.read())
+            self.pattern = json.loads(json_array_string)
+            self.summary += '  Pattern file loaded\n'
+        except IOError:
+            print("  Error : No pattern file found for " + self.pattern_file + '\n')
+            sys.exit()
+
+        return True
 
     def get_summary(self):
         return self.summary
